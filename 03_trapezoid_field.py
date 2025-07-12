131595860bc583393d062b26d1c786d798d2e3b5
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-12 21:57:58 +0200

    Script 03_trapezoid_field

---
+++ 03_trapezoid_field.py
@@ -0,0 +1,5 @@
+a = int(input('Enter the length of the first side of the trapezoid: '))
+b = int(input('Enter the length of the second side of the trapezoid: '))
+h = int(input('Enter the height of the trapezoid: '))
+trapezoid_area = ((a + b) / 2) * h  # Calculate the area of the trapezoid
+print(f'The area of the trapezoid is: {trapezoid_area:.2f}')  # Print the area rounded to two decimal places
