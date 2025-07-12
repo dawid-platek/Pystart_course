b054f930845230c3ef7619a5092077466cedc2fa
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-12 22:25:34 +0200

    Script 05_number_comparison

---
+++ 05_number_comparison.py
@@ -0,0 +1,9 @@
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")
+if number1 > number2:
+    print(f"{number1} is greater than {number2}.")
+elif number1 < number2:
+    print(f"{number1} is less than {number2}.")
+else:
+    print(f"{number1} is equal to {number2}.")
+# This code compares two numbers and prints whether the first number is greater than, less than, or equal to the second number.
