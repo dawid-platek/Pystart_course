40a499ba97a2cb1c11e36d6d94754ee0c354f268
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-12 22:36:17 +0200

    Script 06_temperature_help

---
+++ 06_temperature_help.py
@@ -0,0 +1,8 @@
+temperature = int(input("Enter the temperature in degrees Celsius: "))
+if temperature <= 10:
+    print("Stay at home!"),
+elif 10 < temperature <= 20:
+    print("Wear a jacket!")
+else:
+    print("Have fun!")
+# This code checks the temperature and suggests an action based on the temperature range.
