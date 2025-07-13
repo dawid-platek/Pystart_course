ce02221c163ee3804a932c0001caff69cd2d743e
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-13 21:07:53 +0200

    Script 07_temp_celsius_to_fahrenheit

---
+++ Module 1/07_temp_celsius_to_fahrenheit.py
@@ -0,0 +1,5 @@
+# Przygotuj program, który będzie konwertował temperaturę z Celsjusza na Fahrenheita.
+# Tf = (Tc * 9/5) + 32  # Convert Celsius to Fahrenheit
+temperature_celsius = float(input("Enter the temperature in degrees Celsius: "))
+temperature_fahrenheit = (temperature_celsius * 9/5) + 32  # Convert Celsius to Fahrenheit
+print(f"The temperature in Fahrenheit is: {temperature_fahrenheit:.2f}")  # Print temperature rounded to two decimal places
